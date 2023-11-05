821647261a6d2bde505a44302a8c46a2147c6df3
fedeval98 <fede.val9806@gmail.com>
2023-11-05 07:56:56 -0300

    creados los cards de movies

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -4,9 +4,9 @@ function crearCard (movie){
     return`
     <article class="border-2 border-dashed border-gray-400 rounded-2xl m-4 p-3 flex flex-col">
         <img class="mb-3" src="${movie.image}" alt="${movie.title}"></img>
-        <h3 class="mb-1 text-center font-bold text-2xl ">${movie.title}</h3>
-        <h4 class="mb-3 text-center" >${movie.tagline}</h4>
-        <p class="p-1 mb-3">${movie.overview}</p>
+        <h3 class="mb-1 font-bold text-2xl ">${movie.title}</h3>
+        <h4 class="mb-3" >${movie.tagline}</h4>
+        <p class="mb-3">${movie.overview}</p>
     </article>`
 }
 
